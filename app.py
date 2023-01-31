22a96166bf7a2926f9859374ce99c983817db3f1
Amysoj J A Jithisha <amysojjalouis@gmail.com>
2023-01-31 14:46:01 +0530

    Added Proper Documentation

--- app.py
+++ app.py
@@ -1,48 +1,71 @@
+# Import required libraries for Flask, JSON, and SMTP email
 from flask import Flask, jsonify, request
 import smtplib
-user = "example@email.com"  # Senders email
-pwd = "*********"  # Sender's password
-recipient = "example@email.com"  # Reciever's email
-subject = "Feedback Form"  # Subject Of the email
+
+# Sender's email and password
+user = "example@email.com"
+pwd = "*********"
+
+# Receiver's email
+recipient = "example@email.com"
+
+# Subject of the email
+subject = "Feedback Form"
+
+# Function to send the email
 
 
 def send_email(body, userid):
+    # Set the subject of the email
     SUBJECT = subject
 
-    TEXT = F"{userid} \n\n{body}"  # body of the email to be send
+    # Build the body of the email using the userid and body arguments
+    TEXT = F"{userid} \n\n{body}"
 
-    # message to be send
+    # Build the entire email message
     message = f"From: {user}\nTo:{recipient if isinstance(recipient, list) else [recipient]}\nSubject: {SUBJECT}\n\n{TEXT}"
 
+    # Try to send the email using the Gmail SMTP server
     try:
+        # Connect to Gmail SMTP server
         server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(user, pwd)
-        server.sendmail(user, recipient if isinstance(
-            recipient, list) else [recipient], message)
-        server.close()
+        server.ehlo()  # Identify the server
+        server.starttls()  # Start encrypted connection
+        server.login(user, pwd)  # Login to the email account
+        server.sendmail(user, recipient if isinstance(recipient, list) else [
+                        recipient], message)  # Send the email
+        server.close()  # Close the connection
         print('successfully sent the mail')
+        # Return success message
         return {"status": 'Thank You For the Feedback'}
     except Exception as e:
-        print(e)
+        print(e)  # Print the error if the email fails to send
         print("failed to send mail")
+        # Return failure message
         return {"status": "failed to send Feedback", "Reason": e}
 
 
+# Initialize the Flask application
 app = Flask(__name__)
+
+# Route for sending feedback
 
 
 @app.route('/', methods=['POST'])
 def welcome():
+    # Get the data from the request form
     args = request.form
+    # Check if the request has both userid and body
     if (args.get("userid") != None and args.get("body") != None and args.get("userid") != "" and args.get("body") != ""):
         userid = args.get("userid")
         body = args.get("body")
+        # Call the send_email function with userid and body
         return jsonify(send_email(userid=userid, body=body))
     else:
+        # Return error message if userid or body is missing
         return jsonify({"status": "Couldn't find UserId Or Message Body"})
 
 
+# Run the Flask app if it's the main module
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8000")
--- readme.md
+++ readme.md
@@ -1,9 +1,77 @@
-# Feedback Email Sender #
+# Feedback Email Sender
 
-Hi, I have created an python based emailing sending flask api. Host this in your desired hosting platform and connect to your app or website via api to recieve feedback from your users.
+This is a Flask application that allows users to send feedback to a recipient email that is also owned by you. The recipient email, subject, sender's email and password are set in the code.
 
-PS: I haven't added the CORS support for websites. If you wish to add CORS refer to this docs of [flask_cors](https://flask-cors.readthedocs.io/en/latest/)
+## Getting Started
 
-If you have any doubts contact me at: amysojjalouis@gmail.com
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
-Hope you have a good day, happy coding :)
+## Prerequisites
+
+- Flask: A python micro framework for web development
+- smtplib: A library for sending emails using the Simple Mail Transfer Protocol
+
+## Installing
+
+- Clone the repository to your local machine
+
+```
+$ git clone https://github.com/Amysoj-Louis/Feedback-Email-Sender.git
+```
+
+- Navigate to the cloned repository
+
+```
+$ cd Feedback-Email-Sender
+```
+
+### Configuration
+
+Set the recipient email, subject, sender's email, and password in the code
+
+```
+user = "example@email.com" # Senders email
+pwd = "\***\*\*\*\***" # Sender's password
+recipient = "example@email.com" # Reciever's email
+subject = "Feedback Form" # Subject Of the email
+```
+
+## Running the Application
+
+To start the Flask application, run the following command:
+
+```
+$ python app.py
+```
+
+The application will run on http://0.0.0.0:8000/
+
+## Usage
+
+To send feedback, make a POST request to the / endpoint with the following parameters:
+
+```
+userid: The user's id or email
+body: The feedback message
+```
+
+## Built With
+
+- Flask - A Python micro framework for web development
+- smtplib - A library for sending emails using the Simple Mail Transfer Protocol
+  Contributing
+  Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+<a href="https://github.com/Amysoj-Louis/">
+  <img  alt="Amysoj-Louis" height="50px"width="50px" src="https://avatars.githubusercontent.com/u/100863456?v=4" />
+</a>
+
+[Amysoj J A Louis Joseph]("https://github.com/Amysoj-Louis")
+
+## Acknowledgments
+
+[Flask Docs]("https://flask.palletsprojects.com/en/2.2.x/")
+
+[smtplib Docs]("https://docs.python.org/3/library/smtplib.html)
